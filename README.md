a2a7d4b2c77cf45452f5e665c31745fa8bb37242
schaubes <31410980+schaubes@users.noreply.github.com>
2021-05-20 09:58:38 +0000

    automatic deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+[https://schaubes.dev](https://schaubes.dev)
---
+++ _nuxt/static/1621504715/blog/hello/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621504715",layout:"blog",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhello",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"94b8f06e"}}}}(null));
