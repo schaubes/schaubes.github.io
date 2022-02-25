18d43f64975ae36e2a89885ba08c3ab30bb2262c
schaubes <31410980+schaubes@users.noreply.github.com>
2022-02-25 17:29:50 +0000

    automatic deploy

--- _nuxt/static/1633942781/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633942781",layout:"clean",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"50f3c9eb"}}}}(null,"\u002F"));
