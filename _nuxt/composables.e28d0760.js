68b9751f784c6af28780f1878f1383318ff429d1
schaubes <31410980+schaubes@users.noreply.github.com>
2023-05-23 07:58:03 +0000

    automatic deploy

--- _nuxt/composables.e28d0760.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./entry.8b584669.js";const r=()=>o("color-mode").value;export{r as u};
---
+++ _nuxt/composables.e44dcf35.js
@@ -0,0 +1,1 @@
+import{f as o}from"./entry.ac8d2753.js";const r=()=>o("color-mode").value;export{r as u};
