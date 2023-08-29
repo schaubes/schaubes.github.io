8d3f93c258da8882f88a54f431ef48ff9f212c97
schaubes <31410980+schaubes@users.noreply.github.com>
2023-08-29 08:54:15 +0000

    automatic deploy

---
+++ _nuxt/DocumentDrivenNotFound.58b06dd9.js
@@ -0,0 +1,1 @@
+import{z as n,C as e}from"./entry.cd1da91b.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
--- _nuxt/composables.d1506bb6.js
+++
@@ -1,1 +0,0 @@
-import{G as o}from"./entry.2388b715.js";const r=()=>o("color-mode").value;export{r as u};
