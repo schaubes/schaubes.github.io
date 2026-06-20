db10e43f8c5854de96662d124c6ae00ade0f31ff
schaubes <31410980+schaubes@users.noreply.github.com>
2026-06-20 18:52:05 +0000

    automatic deploy

---
+++ _nuxt/5KGbDD2v.js
@@ -0,0 +1,1 @@
+import{r as e}from"./B_NbOxbv.js";import t from"./CucKJRp6.js";import{C as n,f as s,g as a,e as i,o as p}from"./CuutGUQA.js";import"./BN_7HF1G.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(p(),s(t,{color:"info",icon:i(o).ui.icons.info},{default:a(()=>[e(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
