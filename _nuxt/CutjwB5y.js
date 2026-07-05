9d6f1389090d26180adf1d801830cca6eb67441c
schaubes <31410980+schaubes@users.noreply.github.com>
2026-07-05 09:51:28 +0000

    automatic deploy

--- _nuxt/CutjwB5y.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./DQpHyeJC.js";import a from"./RznBJ_UF.js";import{D as e,f as s,g as t,e as i,o as p}from"./-ZTe1Hi8.js";import"./BN_7HF1G.js";const g={__name:"ProseWarning",setup(c){const o=e();return(r,f)=>(p(),s(a,{color:"warning",icon:i(o).ui.icons.warning},{default:t(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{g as default};
