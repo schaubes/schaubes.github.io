cac5c49400659bb8945349c95f0c4023fab4d297
schaubes <31410980+schaubes@users.noreply.github.com>
2026-07-05 09:28:00 +0000

    automatic deploy

---
+++ _nuxt/wK1cCDue.js
@@ -0,0 +1,1 @@
+import{r as s}from"./DQpHyeJC.js";import e from"./RznBJ_UF.js";import{D as t,f as a,g as p,e as c,o as n}from"./-ZTe1Hi8.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=t();return(r,f)=>(n(),a(e,{color:"success",icon:c(o).ui.icons.tip},{default:p(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
